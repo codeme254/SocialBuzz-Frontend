65e707ae41f4852aea915952146b7c8c6f4c0033
codeme254 <otwomadennis365@gmail.com>
2023-07-10 14:31:54 +0300

    create login form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,51 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+import Title from "../../components/Title/Title";
+
 const Login = () => {
   return (
     <div className="login-container">
-      <h2>This is the login form, login to your account</h2>
+      <Title text="SocialBuzz" />
+      <form action="" className="login-form">
+        <div className="form-group">
+          <div className="form-text-top">
+            <Title text="login into your account" />
+            <p>
+              Login to your account to see photos, videos and updates from your
+              folks
+            </p>
+          </div>
+          <label htmlFor="emailAddress" className="form-group-label">
+            email address
+          </label>
+          <input
+            type="email"
+            id="emailAddress"
+            className="form-group__textual-input"
+            placeholder="email address eg johndoe@gmail.com"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password" className="form-group-label">
+            password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-group__textual-input"
+            placeholder="type a strong password"
+          />
+        </div>
+
+        <button type="submit" className="submit-btn">
+          Login
+        </button>
+
+        <p className="form-guide">
+          Don't have an account? <Link to="/">Sign Up</Link>
+        </p>
+      </form>
     </div>
   );
 };
