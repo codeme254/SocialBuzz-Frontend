53a4c8ace4b40c373a1ae6ef412ff0f4f1e69fe7
codeme254 <otwomadennis365@gmail.com>
2023-07-17 12:08:26 +0300

    start image upload

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvmYyrLCmunT41dfwJXYcI2XKD0PRPNgw",
+  authDomain: "socialbuzz-images.firebaseapp.com",
+  projectId: "socialbuzz-images",
+  storageBucket: "socialbuzz-images.appspot.com",
+  messagingSenderId: "191332868938",
+  appId: "1:191332868938:web:d99e6adc8bde657e5997b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
